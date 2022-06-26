5fbd9333585ef599a9a67278cbbdce94e1eff8aa
samuelowino <samuelowino43@yahoo.com>
2022-06-27 02:38:06 +0300

    init server application

---
+++ gifted-server/src/main/java/com/owino/giftedserver/GiftedServerApplication.java
@@ -0,0 +1,13 @@
+package com.owino.giftedserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftedServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftedServerApplication.class, args);
+	}
+
+}
